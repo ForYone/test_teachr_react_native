134e825e3422603fa2b1ef3ac06bf1a2eb017a00
Yamine  Yakoubi <y.yakoubi.pro@outlook.fr>
2021-02-21 19:44:03 +0100

    modif css

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Carousel from "react-native-carousel-control";
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <View style={{marginBottom: 50,height:190,backgroundColor: '#1E90FF'}}>
-      <Text>Teach'rs favoris</Text>
+      <View style={styles.header}>
+        <Text style={styles.textHeader}>Teach'rs favoris</Text>
       </View>
-      <Carousel  pageStyle={ {backgroundColor: "blue", borderRadius: 10,height : 400} }>
+      <Carousel pageStyle={styles.carousel}>
         <Text>Hello</Text>
         <Text>World!</Text>
         <Text>From carousel</Text>
       </Carousel>
-      
+
     </View>
   );
 }
@@ -25,5 +26,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'center',
   },
- 
+  header: {
+    marginBottom: 50,
+    height: 190,
+    backgroundColor: '#0099FF',
+
+  },
+  textHeader: {
+    position: 'relative',
+    top: 120,
+    marginLeft: 20,
+    color: 'white',
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  carousel: {
+    backgroundColor: "blue",
+    borderRadius: 10,
+    height: 400,
+    width: 250,
+  }
 });
