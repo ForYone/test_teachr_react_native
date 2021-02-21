fc2babe8269f20d62160d5b922fc213abf5dcda2
Yamine  Yakoubi <y.yakoubi.pro@outlook.fr>
2021-02-21 18:59:06 +0100

    debut

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Carousel from "react-native-carousel-control";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{marginBottom: 50,height:190,backgroundColor: '#1E90FF'}}>
+      <Text>Teach'rs favoris</Text>
+      </View>
+      <Carousel  pageStyle={ {backgroundColor: "blue", borderRadius: 10,height : 400} }>
+        <Text>Hello</Text>
+        <Text>World!</Text>
+        <Text>From carousel</Text>
+      </Carousel>
+      
     </View>
   );
 }
@@ -15,7 +23,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+ 
 });
